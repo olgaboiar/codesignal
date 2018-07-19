81244fccbd04cc381e5c47c858c8d17339c916ee
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-07-19 15:10:22 -0400

    added all problems for Always leave the table in order part and Group dishes by type part

---
+++ arcades/db/always_leave_table_in_order/mischievousNephews.sql
@@ -0,0 +1,5 @@
+CREATE PROCEDURE mischievousNephews()
+BEGIN
+    
+	SELECT WEEKDAY(mischief_date) AS weekday, mischief_date, author, title FROM mischief ORDER BY weekday, FIELD(author,'Huey','Dewey','Louie'), mischief_date, title;
+END
