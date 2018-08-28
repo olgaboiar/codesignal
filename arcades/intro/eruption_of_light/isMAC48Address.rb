459b2688e099886681148aaef34809405348d74c
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-08-28 14:00:00 +0100

    completed the Eruption of Light section of the Intro Arcade

---
+++ arcades/intro/eruption_of_light/isMAC48Address.rb
@@ -0,0 +1,14 @@
+def isMAC48Address(inputString)
+    return false if inputString.length != 17
+    for i in (0..inputString.length - 1)
+        if (i + 1) % 3 == 0
+            return false if inputString[i] != "-"
+        else
+            if inputString[i] =~ /^[0-9A-F]/
+                else
+                return false
+            end
+        end
+    end
+    return true
+end
