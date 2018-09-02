8f0a8d96c503b17d7c94ddb5f8995adc1f06def2
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-09-02 14:00:00 +0800

    Finished the Journey Begins section of the Intro arcade

---
+++ arcades/intro/the_journey_begins/checkPalindrome.rb
@@ -0,0 +1,10 @@
+def checkPalindrome(inputString)
+    n = inputString.length
+    for i in (0..n/2)
+        if inputString[i] != inputString[n-1-i]
+            return false
+        end
+        i += 1
+    end
+    return true
+end
