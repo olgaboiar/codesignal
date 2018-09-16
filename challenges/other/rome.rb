b67d1c77a352f8e36d80555b5960c606349c2263
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-09-16 14:00:00 +0800

    Arabic into Roman

---
+++ challenges/other/rome.rb
@@ -0,0 +1,25 @@
+def rome(n)
+    numbers = {
+    1000 => "M",  
+     900 => "CM",  
+     500 => "D",  
+     400 => "CD",
+     100 => "C",  
+      90 => "XC",  
+      50 => "L",  
+      40 => "XL",  
+      10 => "X",  
+        9 => "IX",  
+        5 => "V",  
+        4 => "IV",  
+        1 => "I",  
+  }
+
+    roman = ""
+    numbers.each do |value, letter|
+      roman << letter*(n / value)
+      n = n % value
+    end
+    return roman
+  
+end
